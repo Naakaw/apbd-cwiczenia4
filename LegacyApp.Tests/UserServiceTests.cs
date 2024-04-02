9dabc1086907fa6e0e46cd3ccfcedd28ffd1aa27
Mateusz Wdzienkowski <mateuszwdzienkowski@gmail.com>
2024-04-02 14:46:33 +0200

    Added unit tests for UserService class

--- LegacyApp.Tests/UserServiceTests.cs
+++ LegacyApp.Tests/UserServiceTests.cs
@@ -3,7 +3,7 @@ namespace LegacyApp.Tests;
 public class UserServiceTests
 {
     [Fact]
-    public void AddUser_ReturnsFalseWehnFirstNameIsEmpty()
+    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
         // Arrange
         UserService userService = new UserService();
@@ -55,13 +55,94 @@ public class UserServiceTests
     }
     
     [Fact]
+    public void AddUser_ReturnsFalseWhenYoungerThan21()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser("Jan", "Kowalski", "kowalski@gmail.com", DateTime.Parse("2010-03-21"), 1);
+        
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenVeryImportantClient()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser("Jan", "Malewski", "malewski@gmail.com", DateTime.Parse("1980-01-01"), 2);
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenImportantClient()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser("Jan", "Smith", "smith@gmail.com", DateTime.Parse("1980-01-01"), 3);
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenNormalClient()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser("Jan", "Kwiatkowski", "kwiatkowski@wp.pl", DateTime.Parse("1980-01-01"), 5);
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser("Jan", "Kowalski", "kowalski@wp.pl", DateTime.Parse("1980-01-01"), 5);
+        
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser()
+    {
+        // Arrange
+        UserService userService = new UserService();
+        
+        // Act
+        Action result = () => userService.AddUser("Jan", "Andrzejewicz", "andrzejewicz@wp.pl", DateTime.Parse("1999-03-21"), 6);
+        
+        // Assert
+        Assert.Throws<ArgumentException>(result);
+    }
+    
+    
+    
+    [Fact]
     public void AddUser_ThrowsArgumentExceptionWhenClientDoesNotExist()
     {
         // Arrange
         UserService userService = new UserService();
         
         // Act
-        Action result = () => userService.AddUser("Jan", "Kowalski", "kowalskigmail.com", DateTime.Parse("1999-03-21"), 1);
+        Action result = () => userService.AddUser("Jan", "Kowalski", "kowalski@gmail.com", DateTime.Parse("1999-03-21"), 10);
         
         // Assert
         Assert.Throws<ArgumentException>(result);
